4f9a951b7b255cb3ab8d0645715c572f613279d3
a2montero <74225914+a2montero@users.noreply.github.com>
2020-11-10 01:07:48 -0500

    Update installPython.sh

--- installPython.sh
+++ installPython.sh
@@ -1,12 +1,6 @@
 apt update && apt upgrade
 
-termux-setup-storage
-
-pkg install root-repo
-
-pkg intall unstable-repo
-
-apt install clang python fftw libzmq freetype libpng pkg-config build-essential lib-ssl libgnutls libffi libxml2 libxslt zlib libjpeg-turbo
+apt install clang python fftw libzmq freetype libpng pkg-config build-essential openssl libgnutls libffi libxml2 libxslt zlib libjpeg-turbo
 
 pip install --upgrade pip
 
@@ -17,4 +11,3 @@ pip install kiwisolver
 LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow
 
 CFLAGS=" -I/data/data/com.termux/files/usr/include/freetype2" CPPFLAGS=$CFLAGS LDFLAGS=" -lm -lcompiler_rt" pip install matplotlib
-
